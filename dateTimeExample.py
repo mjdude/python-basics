1d22de1d2f8709a5e196c98a8bb33d3fb5597e26
mjdude <mo@syncspot.net>
2017-05-05 18:42:36 -0700

    added create filename with date

--- dateTimeExample.py
+++ dateTimeExample.py
@@ -1,2 +1,14 @@
 import datetime
-print(datetime.datetime.now())
+
+r"""
+This is a datetime example python script 
+"""
+
+filename=datetime.datetime.now()
+
+def create_file():
+    """This function create an empty file"""
+    with open(filename.strftime('%Y%b%d'), "w") as file:
+        file.write("") #writting empty string
+
+create_file()
