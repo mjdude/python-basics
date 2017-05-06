f93eeb8524d98741adcdd5db8fccabb87ee23027
mjdude <mo@syncspot.net>
2017-05-05 18:49:24 -0700

    added txt exteension to datetime

--- dateTimeExample.py
+++ dateTimeExample.py
@@ -8,7 +8,7 @@ filename=datetime.datetime.now()
 
 def create_file():
     """This function create an empty file"""
-    with open(filename.strftime('%Y%b%d'), "w") as file:
+    with open(filename.strftime('%Y-%m-%d-%M') + ".txt", "w") as file:
         file.write("") #writting empty string
 
 create_file()
